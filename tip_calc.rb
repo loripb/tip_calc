2f2f10c85ea3ab508a217dc3526a224739d061c1
Lori Boyd <lori.p.boyd@gmail.com>
2019-10-26 18:18:44 -0400

    DRY, add 10 & 20 percent calculations, add ability to calculate a custom percentage

--- tip_calc.rb
+++ tip_calc.rb
@@ -8,13 +8,34 @@ class TipCalc
     @tip   = tip
   end
 
-  def calc_15_percent
-    tip = 0.15 * total
+  def total_promt
     grand_total = total + tax + tip
-
     puts "Your tip is #{tip}"
     puts "Here's your Grand total."
     puts "$#{grand_total}"
+  end
+
+  def calc_15_percent
+    @tip = 0.15 * total
+    total_promt
+  end
+
+  def calc_10_percent
+    @tip = 0.10 * total
+    total_promt
+  end
+
+  def calc_20_percent
+    @tip = 0.20 * total
+    total_promt
+  end
+
+  def calc_custom_percent
+    puts "Enter your desired percent amount:"
+    desired_tip = gets.to_f
+
+    @tip = (desired_tip / 100) * total
+    total_promt
   end
 
 end
