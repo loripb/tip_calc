e9ad974e87901a367c4e8011d65a5c7c9e33037c
Lori Boyd <lori.p.boyd@gmail.com>
2019-11-05 01:26:25 -0500

    Remove test method Clean up cached? method Add comments

--- tip_calc.rb
+++ tip_calc.rb
@@ -1,3 +1,4 @@
+# Displays a menu and prompt user for input
 module Menu
   def prompt
       puts "
@@ -32,7 +33,7 @@ module Menu
   end
 
   def change_tip
-    tip_questions
+    tip_options # displays options to cli
 
     case gets.to_i
     when 1
@@ -48,10 +49,13 @@ module Menu
     end
   end
 
+#re-calculates same tip percent with new totals
   def recalc
+    # Removes previous total and tax, then promts user to enter new info
     $user_totals.pop
     $user_totals = [get_total, get_tax]
 
+    # Gets previous selected tip from cache and calculates correct percentage
     case $cached_tip
     when "10"
       percent_10
@@ -66,13 +70,10 @@ module Menu
   end
 
   def start
-    questions
+    questions #reccomends user a tip percent based on questions and gives total
     prompt
   end
 
-  def test_prompt
-    prompt
-  end
 end
 
 module Questionare
@@ -80,7 +81,7 @@ module Questionare
     "Enter: 1-Poor 2-Fair 3-Good 4-Excellent"
   end
 
-  def tip_questions
+  def tip_options
     puts "how much would you like to tip?"
     puts "
       1) 10%
@@ -188,6 +189,7 @@ module Calculations
   end
 end
 
+# Keeps track of last used percentage
 class TipCache
   attr_accessor :tip_cache, :last_used_percentage, :percentage
   def initialize(percentage= 15)
@@ -200,13 +202,13 @@ class TipCache
     last_used_percentage = data.to_s
   end
 
+# If the percentage in the cache, set to last use. Or else store in the cache
   def cached?(percentage)
     if @tip_cache.has_key?(percentage)
-      set_last_used(percentage)
     else
       @tip_cache.store("percentage", percentage)
-      set_last_used(percentage)
     end
+    set_last_used(percentage)
   end
 
   def get_last_used_percentage
@@ -215,6 +217,7 @@ class TipCache
 
 end
 
+# Home of the tip calculations
 class TipCalc
   attr_reader :total, :tax
   attr_accessor :tip
@@ -225,9 +228,9 @@ class TipCalc
     @tip   = tip
   end
 
-  def total_promt
+  def display_totals
     grand_total = total + tax + tip
-    puts "Your tip is $#{tip.ceil(2)}"
+    puts "You'll tip $#{tip.ceil(2)}"
     puts "Here's your grand total."
     puts "$#{grand_total.ceil(2)}"
   end
@@ -239,32 +242,32 @@ class TipCalc
 
   def calc_15_percent
     @tip = 0.15 * total
-    total_promt
+    display_totals
 
     send_tip_to_cache(15)
   end
 
   def calc_10_percent
     @tip = 0.10 * total
-    total_promt
+    display_totals
 
     send_tip_to_cache(10)
   end
 
   def calc_20_percent
     @tip = 0.20 * total
-    total_promt
+    display_totals
 
     send_tip_to_cache(20)
   end
 
   def calc_custom_percent
     puts "Enter your desired percent amount:"
-    percentage = gets
+    percentage = gets.to_i
     TipCache.new(percentage)
 
     @tip = ( percentage.to_f / 100) * total
-    total_promt
+    display_totals
 
     send_tip_to_cache(percentage)
   end
